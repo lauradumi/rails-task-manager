eed8d699aa27dc6e3e05b84c72b4ed78fb929f9e
Laura Dumitriu <lauradumitriu01@gmail.com>
2024-04-11 20:00:16 -0400

    rails task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    @task.save # Will raise ActiveModel::ForbiddenAttributesError
+    @task.save
     redirect_to tasks_path(@task)
   end
 
@@ -21,15 +21,22 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get 'tasks/:id', to: 'tasks#show', as: :task
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
   patch 'tasks/:id', to: 'tasks#update', as: :update
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
 end
