f82ff59901713bd05eb2a26ed5be37a4f4b20fb9
Laura Dumitriu <lauradumitriu01@gmail.com>
2024-06-24 22:07:49 -0400

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+root to: 'tasks#home'
   get 'tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new', as: :new
   post 'tasks', to: 'tasks#create', as: :create
